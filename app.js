6bc66eb63d0b68e0ce3ada8f760fd5fe0ab4b5e5
shubham-369 <shubham.srivastav666@gmail.com>
2023-09-14 18:36:03 +0530

    backendreadytodeploy

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const sequelize = require('./util/database');
 const helmet = require('helmet');
 const fs = require('fs');
 const morgan = require('morgan');
 require('dotenv').config();
+const sequelize = require('./util/database');
 
 const userRoutes = require('./routes/user');
 const premiumRoutes = require('./routes/premium');
